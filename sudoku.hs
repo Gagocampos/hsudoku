41eee59f14630c04ee8daac94f6b46605f10b30e
Kuassi Franck kumako <franckkumako@gmail.com>
2019-05-14 13:06:05 -0300

    alteracoes feitas

--- sudoku.hs
+++ sudoku.hs
@@ -5,6 +5,151 @@ import Data.Function
 import Control.Monad
 import Control.Applicative
 
+
+--cell 
+data Celula = Fixed Int | Possible [Int] deriving (Show, Eq)
+--Row  
+type Linha  = [Celula]
+--Grid 
+type Grade = [Linha]
+
+
+
+
+mostrarGrade :: Grade -> String
+mostrarGrade = unlines . map (unwords . map mostrarCelula)
+  where
+    mostrarCelula (Fixed x) = show x
+    mostrarCelula _ = "."
+
+
+
+--readGrid :: String -> Maybe Grid
+lerGrade :: String -> Maybe Grade
+lerGrade s  
+  | length s == 81 = traverse (traverse lerCelula) . Data.List.Split.chunksOf 9 $ s
+  | otherwise      = Nothing
+  where
+    lerCelula '.' = Just $ Possible [1..9]
+    lerCelula c
+      | Data.Char.isDigit c && c > '0' = Just . Fixed . Data.Char.digitToInt $ c
+      | otherwise = Nothing
+
+      
+podaCelulas :: [Celula] -> Maybe [Celula]
+podaCelulas celulas = traverse podaCelula celulas
+  where
+    fixeds = [x | Fixed x <- celulas]
+    podaCelula (Possible xs) = case xs Data.List.\\ fixeds of
+      []  -> Nothing
+      [y] -> Just $ Fixed y
+      ys  -> Just $ Possible ys
+    podaCelula x = Just x
+
+
+  
+mostrarGradeComPossibilidades :: Grade -> String
+mostrarGradeComPossibilidades = unlines . map (unwords . map mostrarCelula)
+  where
+    mostrarCelula (Fixed x)     = show x ++ "          "
+    mostrarCelula (Possible xs) =
+      (++ "]")
+      . Data.List.foldl' (\acc x -> acc ++ if x `elem` xs then show x else " ") "["
+      $ [1..9]
+  
+
+subGradesParaLinhas :: Grade -> Grade 
+subGradesParaLinhas =
+  concatMap (\rows -> let [r1, r2, r3] = map (Data.List.Split.chunksOf 3) rows
+                      in zipWith3 (\a b c -> a ++ b ++ c) r1 r2 r3)
+  . Data.List.Split.chunksOf 3
+
+podaGrade' :: Grade -> Maybe Grade
+podaGrade' grade =
+  traverse podaCelulas grade
+  >>= fmap Data.List.transpose . traverse podaCelulas . Data.List.transpose
+  >>= fmap subGradesParaLinhas . traverse podaCelulas . subGradesParaLinhas
+
+
+
+proximosGrades :: Grade -> (Grade, Grade)
+proximosGrades grade =
+  let (i, first@(Fixed _), rest) =
+        fixCell
+        . Data.List.minimumBy (compare `Data.Function.on` (possibilityCount . snd))
+        . filter (isPossible . snd)
+        . zip [0..]
+        . concat
+        $ grid
+  in (replace2D i first grid, replace2D i rest grid)
+  where
+    isPossible (Possible _) = True
+    isPossible _            = False
+    possibilityCount (Possible xs) = length xs
+    possibilityCount (Fixed _)     = 1
+    fixCell (i, Possible [x, y]) = (i, Fixed x, Fixed y)
+    fixCell (i, Possible (x:xs)) = (i, Fixed x, Possible xs)
+    fixCell _                    = error "Caso impossível"
+    replace2D :: Int -> a -> [[a]] -> [[a]]
+    replace2D i v =
+      let (x, y) = (i `quot` 9, i `mod` 9) in replace x (replace y (const v))
+    replace p f xs = [if i == p then f x else x | (x, i) <- zip xs [0..]]
+
+  
+podaGrade :: Grade -> Maybe Grade
+podaGrade = fixM podaGrade'
+  where
+    fixM f x = f x >>= \x' -> if x' == x then return x else fixM f x'
+
+resolve :: Grade -> Maybe Grade
+resolve grid = podaGrade grid >>= resolve'
+  where
+    resolve' g
+      | isGridInvalid g = Nothing
+      | gradecheio  g  = Just g
+      | otherwise       =
+          let (grid1, grid2) = nextGrids g
+          in resolve grid1 <|> resolve grid2
+
+
+
+gradecheio :: Grade -> Bool  
+gradecheio  grid = null [ () | Possible _ <- concat grid ]
+isGridInvalid :: Grade -> Bool
+isGridInvalid grade =
+  any isInvalidRow grade
+  || any isInvalidRow (Data.List.transpose grade)
+  || any isInvalidRow (subGridsToRows grade)
+  where
+    isInvalidRow row =
+      let fixeds         = [x | Fixed x <- row]
+          emptyPossibles = [x | Possible x <- row, null x]
+      in hasDups fixeds || not (null emptyPossibles)
+    hasDups l = hasDups' l []
+    hasDups' [] _ = False
+    hasDups' (y:ys) xs
+      | y `elem` xs = True
+      | otherwise   = hasDups' ys (y:xs)
+
+main :: IO ()
+main = do
+    inputs <- lines <$> getContents
+    Control.Monad.forM_ inputs $ \input ->
+        case lerGrade input of
+            Nothing   -> putStrLn "Entrada invalida"
+            Just grid -> case resolve grid of
+                Nothing    -> putStrLn "Solução não encontrado"
+                Just grid' -> putStrLn $ mostrarGrade'
+
+
+{--
+import Data.List.Split
+import Data.Char
+import Data.List
+import Data.Function
+import Control.Monad
+import Control.Applicative
+
 data Cell = Fixed Int | Possible [Int] deriving (Show, Eq)
 	
 type Row  = [Cell]
@@ -124,3 +269,7 @@ main = do
             Just grid -> case solve grid of
                 Nothing    -> putStrLn "No solution found"
                 Just grid' -> putStrLn $ showGrid grid'
+
+
+
+              --}
